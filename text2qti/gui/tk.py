dee5f0dccb1e290908491227029791fd075c2fcd
dlehman83 <dlehman83@gmail.com>
2020-12-22 10:11:39 -0500

    Update tk.py
    Removed advanced options, not tested for Schoology.

--- text2qti/gui/tk.py
+++ text2qti/gui/tk.py
@@ -29,7 +29,7 @@ def main():
     file_name = ''
 
     window = tk.Tk()
-    window.title('text2qti')
+    window.title('text2CC')
     # Bring window to front and put in focus
     window.iconify()
     window.update()
@@ -42,24 +42,24 @@ def main():
 
     header_label = tk.Label(
         window,
-        text='text2qti – Create quizzes in QTI format from Markdown-based plain text',
+        text='text2cc – Create quizzes in Common Cartridge format from Markdown-based plain text',
         font=(None, 16),
     )
     header_label.grid(
         row=current_row, column=0, columnspan=column_count, padx=(30, 30),
         sticky='nsew',
     )
-    current_row += 1
-    header_link_label = tk.Label(
-        window,
-        text='github.com/gpoore/text2qti',
-        font=(None, 14), fg='blue', cursor='hand2',
-    )
-    header_link_label.bind('<Button-1>', lambda x: webbrowser.open_new('https://github.com/gpoore/text2qti'))
-    header_link_label.grid(
-        row=current_row, column=0, columnspan=column_count, padx=(30, 30),
-        sticky='nsew',
-    )
+    #current_row += 1
+    # header_link_label = tk.Label(
+        # window,
+        # text='github.com/gpoore/text2qti',
+        # font=(None, 14), fg='blue', cursor='hand2',
+    # )
+    # header_link_label.bind('<Button-1>', lambda x: webbrowser.open_new('https://github.com/gpoore/text2qti'))
+    # header_link_label.grid(
+        # row=current_row, column=0, columnspan=column_count, padx=(30, 30),
+        # sticky='nsew',
+    # )
     current_row += 1
     version_label = tk.Label(
         window,
@@ -113,101 +113,101 @@ def main():
     )
     current_row += 1
 
-
-    advanced_options_label = tk.Label(
-        window,
-        text='Advanced options – LaTeX math & executable code',
-        justify='right',
-    )
-    advanced_options_label.grid(
-        row=current_row, column=1, columnspan=2, padx=(0, 0), pady=(5, 5),
-        sticky='nsw',
-    )
-    current_row += 1
-
-
-    latex_url_label = tk.Label(
-        window,
-        text='LaTeX math rendering URL:\n(for Canvas and similar systems)',
-        justify='right',
-    )
-    latex_url_label.grid(
-        row=current_row, column=0, padx=(30, 5), pady=(5, 5),
-        sticky='nse',
-    )
-    latex_url_entry = tk.Entry(window, width=100)
-    latex_url_entry.grid(
-        row=current_row, column=1, columnspan=column_count-1, padx=(0, 30), pady=(5, 5),
-        sticky='nsew',
-    )
-    if 'latex_render_url' in config:
-        latex_url_entry.insert(1, f"{config['latex_render_url']}")
-    current_row += 1
+ #Above here set good
+    # advanced_options_label = tk.Label(
+        # window,
+        # text='Advanced options – LaTeX math & executable code',
+        # justify='right',
+    # )
+    # advanced_options_label.grid(
+        # row=current_row, column=1, columnspan=2, padx=(0, 0), pady=(5, 5),
+        # sticky='nsw',
+    # )
+    # current_row += 1
 
 
-    pandoc_exists = bool(shutil.which('pandoc'))
-    pandoc_mathml_label = tk.Label(
-        window,
-        text='Convert LaTeX math to MathML:\n(requires Pandoc; ignores rendering URL)',
-        justify='right',
-    )
-    if not pandoc_exists:
-        pandoc_mathml_label['fg'] = 'gray'
-    pandoc_mathml_label.grid(
-        row=current_row, column=0, padx=(30, 5), pady=(5, 5),
-        sticky='nse',
-    )
-    pandoc_mathml_bool = tk.BooleanVar()
-    def pandoc_mathml_command():
-        if pandoc_mathml_bool.get():
-            latex_url_label['fg'] = 'gray'
-            latex_url_entry['fg'] = 'gray'
-        else:
-            latex_url_label['fg'] = 'black'
-            latex_url_entry['fg'] = 'black'
-    if pandoc_exists:
-        pandoc_mathml_button = tk.Checkbutton(
-            window,
-            variable=pandoc_mathml_bool,
-            command=pandoc_mathml_command,
-        )
-        pandoc_mathml_bool.set(config['pandoc_mathml'])
-    else:
-        pandoc_mathml_button = tk.Checkbutton(
-            window,
-            state=tk.DISABLED,
-        )
-    pandoc_mathml_button.grid(
-        row=current_row, column=1, sticky='w',
-    )
-    current_row += 1
+    # latex_url_label = tk.Label(
+        # window,
+        # text='LaTeX math rendering URL:\n(for Canvas and similar systems)',
+        # justify='right',
+    # )
+    # latex_url_label.grid(
+        # row=current_row, column=0, padx=(30, 5), pady=(5, 5),
+        # sticky='nse',
+    # )
+    # latex_url_entry = tk.Entry(window, width=100)
+    # latex_url_entry.grid(
+        # row=current_row, column=1, columnspan=column_count-1, padx=(0, 30), pady=(5, 5),
+        # sticky='nsew',
+    # )
+    # if 'latex_render_url' in config:
+        # latex_url_entry.insert(1, f"{config['latex_render_url']}")
+    # current_row += 1
 
 
-    run_code_blocks_label = tk.Label(
-        window,
-        text='Allow executable code blocks:\n(only use for trusted code)',
-        justify='right',
-    )
-    run_code_blocks_label.grid(
-        row=current_row, column=0, padx=(30, 5), pady=(5, 5),
-        sticky='nse',
-    )
-    run_code_blocks_bool = tk.BooleanVar()
-    run_code_blocks_bool.set(config['run_code_blocks'])
-    def run_code_blocks_command():
-        if run_code_blocks_bool.get():
-            run_code_blocks_label['fg'] = 'red'
-        else:
-            run_code_blocks_label['fg'] = 'black'
-    run_code_blocks_button = tk.Checkbutton(
-        window,
-        variable=run_code_blocks_bool,
-        command=run_code_blocks_command,
-    )
-    run_code_blocks_button.grid(
-        row=current_row, column=1, sticky='w',
-    )
-    current_row += 1
+    # pandoc_exists = bool(shutil.which('pandoc'))
+    # pandoc_mathml_label = tk.Label(
+        # window,
+        # text='Convert LaTeX math to MathML:\n(requires Pandoc; ignores rendering URL)',
+        # justify='right',
+    # )
+    # if not pandoc_exists:
+        # pandoc_mathml_label['fg'] = 'gray'
+    # pandoc_mathml_label.grid(
+        # row=current_row, column=0, padx=(30, 5), pady=(5, 5),
+        # sticky='nse',
+    # )
+    # pandoc_mathml_bool = tk.BooleanVar()
+    # def pandoc_mathml_command():
+        # if pandoc_mathml_bool.get():
+            # latex_url_label['fg'] = 'gray'
+            # latex_url_entry['fg'] = 'gray'
+        # else:
+            # latex_url_label['fg'] = 'black'
+            # latex_url_entry['fg'] = 'black'
+    # if pandoc_exists:
+        # pandoc_mathml_button = tk.Checkbutton(
+            # window,
+            # variable=pandoc_mathml_bool,
+            # command=pandoc_mathml_command,
+        # )
+        # pandoc_mathml_bool.set(config['pandoc_mathml'])
+    # else:
+        # pandoc_mathml_button = tk.Checkbutton(
+            # window,
+            # state=tk.DISABLED,
+        # )
+    # pandoc_mathml_button.grid(
+        # row=current_row, column=1, sticky='w',
+    # )
+    # current_row += 1
+
+
+    # run_code_blocks_label = tk.Label(
+        # window,
+        # text='Allow executable code blocks:\n(only use for trusted code)',
+        # justify='right',
+    # )
+    # run_code_blocks_label.grid(
+        # row=current_row, column=0, padx=(30, 5), pady=(5, 5),
+        # sticky='nse',
+    # )
+    # run_code_blocks_bool = tk.BooleanVar()
+    # run_code_blocks_bool.set(config['run_code_blocks'])
+    # def run_code_blocks_command():
+        # if run_code_blocks_bool.get():
+            # run_code_blocks_label['fg'] = 'red'
+        # else:
+            # run_code_blocks_label['fg'] = 'black'
+    # run_code_blocks_button = tk.Checkbutton(
+        # window,
+        # variable=run_code_blocks_bool,
+        # command=run_code_blocks_command,
+    # )
+    # run_code_blocks_button.grid(
+        # row=current_row, column=1, sticky='w',
+    # )
+     #current_row += 1
 
 
     def run():
@@ -222,10 +222,10 @@ def main():
             run_message_text.insert(tk.INSERT, error_message)
             run_message_text['fg'] = 'red'
             return
-        if latex_url_entry.get():
-            config['latex_render_url'] = latex_url_entry.get()
-        config['run_code_blocks'] = run_code_blocks_bool.get()
-        config['pandoc_mathml'] = pandoc_mathml_bool.get()
+        #if latex_url_entry.get():
+            #config['latex_render_url'] = latex_url_entry.get()
+        #config['run_code_blocks'] = run_code_blocks_bool.get()
+        #config['pandoc_mathml'] = pandoc_mathml_bool.get()
 
         file_path = pathlib.Path(file_name)
         try:
